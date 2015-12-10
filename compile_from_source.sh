63bda18f221b4b95c9964c377a7cebbf9be6bcb2
Ubuntu <ubuntu@ip-172-31-22-204.ec2.internal>
2015-12-10 22:06:45 +0000

    fixed cloned wrong repository in set up script

--- compile_from_source.sh
+++ compile_from_source.sh
@@ -4,7 +4,7 @@
 # adapted from http://bitzuma.com/posts/compile-bitcoin-core-from-source-on-ubuntu/
 
 mkdir -p src && cd src
-git clone https://github.com/Dellvan7/measurespv.git
+git clone https://github.com/Dellvan7/bitcoin.git
 sudo apt-get install build-essential
 wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
 echo '12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef  db-4.8.30.NC.tar.gz' | sha256sum -c
